179169264ed122cb28acde7d4001bd9744f44675
SimicSvetislav <simicsvetislav@gmail.com>
2020-04-21 22:55:22 +0200

    Ispravljen prikaz izvornog koda pametnog ugovora kada se projekat pokreće iz jar fajla

--- backend/src/main/java/rs/ac/uns/ftn/informatics/legal_tech/allotment/controllers/FilesController.java
+++ backend/src/main/java/rs/ac/uns/ftn/informatics/legal_tech/allotment/controllers/FilesController.java
@@ -1,8 +1,11 @@
 package rs.ac.uns.ftn.informatics.legal_tech.allotment.controllers;
 	
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import rs.ac.uns.ftn.informatics.legal_tech.allotment.AllotmentApplication;
+
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/files")
@@ -28,27 +33,22 @@ public class FilesController {
 	    
 		String sourceCode = "";
 		
-	    Resource resource = new ClassPathResource("Allotment.sol");
-	    
-	    File file = null;
+	    // Resource resource = new ClassPathResource("Allotment.sol");
+		InputStream is = AllotmentApplication.class.getClassLoader().getResourceAsStream("Allotment.sol");
+		
+	    /*File file = null;
 	    try {
 			file = resource.getFile();
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occured");
-		}
+		}*/
 	    
-	    Scanner reader = null;
-	    try {
-			reader = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occured");
-		}
+	    Scanner reader = new Scanner(is);
 	    
 	    while (reader.hasNextLine()) {
 	    	String line = reader.nextLine();
-	    	System.out.println(line);
+	    	// System.out.println(line);
 	    	sourceCode += line + "\n";
 	    }
 	    
