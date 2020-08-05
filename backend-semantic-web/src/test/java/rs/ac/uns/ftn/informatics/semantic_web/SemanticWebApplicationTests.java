10de55ee28cd7b503fdeb612315ca3f7780fc17d
SimicSvetislav <simicsvetislav@gmail.com>
2020-08-05 15:07:32 +0200

    Inicijalni backend za Semantički veb

---
+++ backend-semantic-web/src/test/java/rs/ac/uns/ftn/informatics/semantic_web/SemanticWebApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.informatics.semantic_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanticWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
